bd2a1b78f94122fb6323434d2dfcaa2198a170d3
suhyun9764 <suhyun9764@naver.com>
2024-02-29 15:05:59 +0900

    feat(): Memo create,read 구현

---
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -0,0 +1,20 @@
+package com.sparta.memo.entity;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Memo {
+    private Long id;
+    private String username;
+    private String contents;
+
+    public Memo(MemoRequestDto memorequestDto) {
+        this.username = memorequestDto.getUsername();
+        this.contents = memorequestDto.getContents();
+    }
+}
